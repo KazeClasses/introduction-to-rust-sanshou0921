299abcb03f6922a466131097efb7d462c251b285
sanshou0921 <shianjie009@gmail.com>
2024-09-26 20:08:12 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+// Import our outputted wasm ES6 module
+// Which, export default's, an initialization function
+import init from "./pkg/mcmc_example.js";
+const runWasm = async () => {
+ // Instantiate our wasm module
+ const helloWorld = await init("./pkg/mcmc_example_bg.wasm");
+ // Call the run function in the wasm module, which will return the result of our
+  calculation
+ helloWorld.run();
+};
+runWasm();
 
